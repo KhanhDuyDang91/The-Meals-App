6073239b3275cabcb1a4d7d315c440b47e3294eb
KhanhDuyDang91 <khanhduydang910@gmail.com>
2023-03-22 16:42:52 +0700

    data constructor

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,20 +1,27 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button, FlatList } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
-import { Button } from "react-native";
 
 import CategoryMealsScreen from "./CategoryMealsScreen";
+import { CATEGORIES } from "../data/dummy-data";
+
+const renderGridItem = (itemData) => {
+  return (
+    <View style={styles.gridItem}>
+      <Text>{itemData.item.title}</Text>
+    </View>
+  );
+};
 
 const CategoriesScreen = (props) => {
   return (
-    <View style={styles.screen}>
-      <Text>This is HomeScreen</Text>
-      <Button
-        title="Details"
-        onPress={() => props.navigation.navigate("Meal")}
-      />
-    </View>
+    <FlatList
+      keyExtractor={(item, index) => item.id}
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2}
+    />
   );
 };
 
@@ -23,6 +30,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
+    backgroundColor: "red",
   },
 });
 
