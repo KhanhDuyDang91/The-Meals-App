de7b64bfd138d7eece96ef14118c04c8398d310a
KhanhDuyDang91 <khanhduydang910@gmail.com>
2023-03-28 18:43:17 +0700

    Fixing the Shadow

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -35,16 +35,19 @@ const styles = StyleSheet.create({
     margin: 15,
     height: 150,
     borderRadius: 8,
-    overflow: "hidden",
-  },
-  container: {
-    flex: 1,
-    borderRadius: 8,
+    overflow:
+      Platform.OS === "android" && Platform.Version >= 21
+        ? "hidden"
+        : "visible",
+    elevation: 6,
     shadowColor: "black",
     shadowOpacity: 0.26,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 10,
-    elevation: 6,
+  },
+  container: {
+    flex: 1,
+    borderRadius: 8,
     padding: 15,
     justifyContent: "flex-end",
     alignItems: "flex-end",
